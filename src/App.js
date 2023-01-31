5f293b1fcaf4e16c1b44b26dcb4021e931bc7e06
sakti8093 <saktiprasad8093@gmail.com>
2023-01-31 13:54:11 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,52 @@
-import React from 'react';
-import './styles.css';
+import React, { useState } from "react";
+import "./styles.css";
 
 function App() {
+  const [state, setState] = useState({
+    input: "",
+    checked: false,
+    data: "",
+    error: false,
+    errorMessage: "",
+  });
+
+  const handleChange = (e) => {
+    setState({ ...state, input: e.target.value });
+  };
+
+  const handleCheck = (e) => {
+    setState({ ...state, checked: e.target.checked });
+  };
+
+  const handleClick = async () => {
+    try {
+      let response = await fetch(
+        `https://api.github.com/users/${state.input}/repos`,
+        {
+          Accept: "application/vnd.github+json",
+        }
+      )
+      response = await response.json();
+      if (response.message) {
+        setState({ ...state, error: true, errorMessage: response.message });
+      }else{    
+        setState({ ...state, data: response });
+      }
+    } catch (err) {
+      setState({ ...state, error: true, errorMessage: err.message });
+    }
+  };
+
   return (
     <div className="App">
       <div className="input">
         <label htmlFor="username">Github username: </label>
-        <input id="username" type="text" />
+        <input onChange={handleChange} id="username" type="text" />
         <label htmlFor="fork">Include forks: </label>
-        <input id="fork" type="checkbox" />
-        <button>Submit</button>
+        <input onChange={handleCheck} id="fork" type="checkbox" />
+        <button disabled={state.input.length === 0} onClick={handleClick}>
+          Submit
+        </button>
       </div>
       <section>
         <header>
@@ -18,14 +55,33 @@ function App() {
           <div className="col">Description</div>
           <div className="col">Size</div>
         </header>
-        <div>
-          <div className="col">C1</div>
-          <div className="col">C2</div>
-          <div className="col">C3</div>
-          <div className="col">C4</div>
-        </div>
+        {state.error
+          ? ""
+          : state.data
+          ? state.checked
+            ? state.data.map((elem) => (
+                <div key={elem.id}>
+                  <div className="col">{elem.full_name}</div>
+                  <div className="col">{elem.language}</div>
+                  <div className="col">{elem.description}</div>
+                  <div className="col">{elem.size}</div>
+                </div>
+              ))
+            : state.data
+                .filter((elem) => {
+                  return elem.fork !== true;
+                })
+                .map((elem) => (
+                  <div key={elem.id}>
+                    <div className="col">{elem.full_name}</div>
+                    <div className="col">{elem.language}</div>
+                    <div className="col">{elem.description}</div>
+                    <div className="col">{elem.size}</div>
+                  </div>
+                ))
+          : ""}
       </section>
-      <div className="error">Dummy error</div>
+      {state.error ? <div className="error">{state.errorMessage}</div> : ""}
     </div>
   );
 }
